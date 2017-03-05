511ee3574e00e70e42386a84db476711a99ab5b7
cbgillis <c.gillis18@yahoo.com>
2017-03-05 03:04:56 -0700

    Update EventSeatingMap.java
    added logic for airline layout creation

--- EventSeatingMap.java
+++ EventSeatingMap.java
@@ -14,7 +14,6 @@ public class EventSeatingMap{
 	private double vipDiscount = 0.80;	//multiply to apply discount
 	private double bulkDiscount = 0.90;	//multiply to apply discount
 	private int numSeatsSelected = 0;	// number of seats user picks
-	
 	private int numSeatsRemaining; // optional
 	
 	/* Creates pop-up to confirm purchase total*/
@@ -72,8 +71,45 @@ public class EventSeatingMap{
 	}
 	/* Creates airline layout*/
 	private void createAirline(){
+		JFrame planeLayout = new JFrame();
+		JPanel panel = new JPanel();
+		public Seats[][] seats = new Seats[6][12];//creates a 2d array of uninitialized seat buttons
+		planeLayout.setTitle("Airplane");
+      		setVisible(true);
+      		panel.setLayout(new GridLayout(6,12));
+      		setSize(900,400);
+      		setResizable(false);
 		
-	}
+		for(int i = 0; i < 6; i++)
+        		for(int j = 0; j < 12; j++)
+     		 {
+       			 if(j < 4 && i != 3)
+			{
+				seats[i][j] = new Seats(200.00, "A" + ij + "");//first class
+				seats[i][j].addActionListener(this);
+				panel.add(seats[i][j]);
+			}	
+			if((j >= 4 && j < 8) && i != 3)
+			{
+				seats[i][j] = new Seats(150.00, "B" + ij + "");//business
+				seats[i][j].addActionListener(this);
+				panel.add(seats[i][j]);
+			}
+			if(j > 8 && i != 3)
+			{
+				seats[i][j] = new Seats(100.00, "C" + ij + "");//economy
+				seats[i][j].addActionListener(this);
+				panel.add(seats[i][j]);
+			}
+			if(i == 3)
+			{
+				seats[i][j] = new Seats(0, "");//emulates the row between seats on a plane
+				seats[i][j].setEnabled(false);
+				seats[i][j].setAvailable(false);
+			}
+		add(panel);
+      		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		}//haven't tested if the name and stuff shows properly, i tested using my TTT and it looked like the format i expected but modifying it in the same way, so the logic SHOULD be correct lol.CG
 	/* When a seat is clicked and selected.*/
 	private class ButtonClickListener implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
