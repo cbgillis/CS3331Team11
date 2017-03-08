d318f1eff6c00ff393a028d68327d6e42b0e0e15
cwtovar <chriswtovar@gmail.com>
2017-03-07 18:38:25 -0700

    Update EventSeatingMap.java

--- EventSeatingMap.java
+++ EventSeatingMap.java
@@ -1,27 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.border.LineBorder;
 import java.awt.event.*;
 import java.util.Scanner; 
 
 public class EventSeatingMap{
 	
-	private boolean isVIP = false;		// true if VIP transaction
-	private boolean isBulk = false;		// true if numSeats > 4
-	private int[] vipMembers = {159,7913,753};	//dummy values of VIP members
-	private String title = "Seat Selection";	//title of seat selection window
-	private double currTotal = 0;	// total as seats are being selected
-	private double finalPrice = 0;	// absolute final price
-	private double vipDiscount = 0.80;	//multiply to apply discount
-	private double bulkDiscount = 0.90;	//multiply to apply discount
-	private int numSeatsSelected = 0;	// number of seats user picks
-	private int numSeatsRemaining; // optional
+	 private boolean isVIP = false;		// true if VIP transaction
+	 private boolean isBulk = false;		// true if numSeats > 4
+	 private int[] vipMembers = {159,7913,753};	//dummy values of VIP members
+	 private String title = "Seat Selection";	//title of seat selection window
+	 private double currTotal = 0;	// total as seats are being selected
+	 private double finalPrice = 0;	// absolute final price
+	 private double vipDiscount = 0.80;	//multiply to apply discount
+	 private double bulkDiscount = 0.90;	//multiply to apply discount
+	 private int numSeatsSelected = 0;	// number of seats user picks
+	 private Scanner scnr = new Scanner(System.in);
+	
+	 int numSeatsRemaining; // optional
 	
 	/* Creates pop-up to confirm purchase total*/
 	private void displayPrice(){
-		if(isVip){
+		if(this.isVIP){
 			finalPrice = currTotal * vipDiscount;
 		}
-		else if(isBulk){
+		else if(this.isBulk){
 			finalPrice = currTotal * bulkDiscount;
 		}
 		JFrame checkout = new JFrame("Verify Checkout Details ");
@@ -37,21 +40,45 @@ public class EventSeatingMap{
 	}
 	/* Totals up cost for seats selected before any discounts.*/
 	private void addSeat(){
-		currTotal += seats[x].getPrice();
+		//currTotal += seats[x].getPrice();
 	}
 	/* Gets the event the user is attending*/
 	private void getEvent(){
+		System.out.println("1. Movie Theater");
+		System.out.println("2. Stage Theatre");
+		System.out.println("3. Airline");
+		System.out.println("");
+		System.out.println("Enter the event you would like to attend: ");
+		int choice = scnr.nextInt();
+		
+		switch(choice){
+			case 1: choice = 1;
+			createMovieTheater();
+			break;
+			
+			case 2: choice = 2;
+			createStageTheatre();
+			break;
+			
+			case 3: choice = 3;
+			createAirline();
+			break;
+			
+			default:
+			System.out.println("Choice not recognized / menu option not available.");
+		
+		}
 		
 	}
 	/* Determines if Bulk Purchase*/
 	private void checkBulk(){
 		if(numSeatsSelected > 4){
-			isbulk = true;
+			this.isBulk = true;
 		}
 	}
 	/* Checks if a valid VIP user is cheking out*/
 	private void checkVip(){
-		Scanner scnr = new Scanner(System.in);
+		//Scanner scnr = new Scanner(System.in);
 		System.out.print("Enter VIP verification code: ");
 		int ID = scnr.nextInt();
 		for(int i = 0; i < vipMembers.length; i++){
@@ -64,7 +91,7 @@ public class EventSeatingMap{
 	/* Creates movie theater layout*/
 	private void createMovieTheater(){
 		int seatCounter = 0;							//Counter for the seats.
-		Seats[] seats = new Seats[50];						//The array to keep the seats.
+		Seat[] seats = new Seat[50];						//The array to keep the seats.
 		JPanel panel = new JPanel();						//The panel for the seats.
 		JPanel screen = new JPanel();						//The panel for the screen.
 		JLabel sc = new JLabel("SCREEN");					//The label for the screen.
@@ -75,21 +102,22 @@ public class EventSeatingMap{
 		//For loop to add the seats to the panel and the space between the seats.
 		for(int i = 0; i < 60; i++){
 			if(i < 15){				//The cheapest seats available 0-14.
-				seats[seatCounter] = new Seats(5.00, "a" + i);
+				seats[seatCounter] = new Seat(5.00, "a" + i);
+				seats[seatCounter].setText(seats[seatCounter].getSeatName());
 				panel.add(seats[seatCounter++]);
 			}
 			if(i >= 15 && i < 20){			//Space between cheapest seats and the more expensive seats.
-				panel.add(new JLabel(""));
+				panel.add(new JLabel());
 			}
 			if(i >= 20 && i < 45){			//The more expensive seats 15-40.
-				seats[seatCounter] = new Seats(8.00, "a" + i);
+				seats[seatCounter] = new Seat(8.00, "a" + i);
 				panel.add(seats[seatCounter++]);
 			}
 			if(i >= 45 && i < 50){			//Space between more expensive seats and the most expensive seats.
 				panel.add(new JLabel(""));
 			}
 			if(i >= 50 && i < 60){			//The most expensive seats 40-49.
-				seats[seatCounter] = new Seats(10.00, "a" + i);
+				seats[seatCounter] = new Seat(10.00, "a" + i);
 				panel.add(seats[seatCounter++]);
 			}
 		}
@@ -104,6 +132,7 @@ public class EventSeatingMap{
 		frame.add(new JPanel(), BorderLayout.EAST);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
 	/* Creates stage theatre layout*/
 	private void createStageTheatre(){
@@ -113,50 +142,71 @@ public class EventSeatingMap{
 	private void createAirline(){
 		JFrame planeLayout = new JFrame();
 		JPanel panel = new JPanel();
-		public Seats[][] seats = new Seats[6][12];//creates a 2d array of uninitialized seat buttons
+		Seat[][] seats = new Seat[6][12];//creates a 2d array of uninitialized seat buttons
 		planeLayout.setTitle("Airplane");
-      		setVisible(true);
+      		planeLayout.setVisible(true);
       		panel.setLayout(new GridLayout(6,12));
-      		setSize(900,400);
-      		setResizable(false);
-		
-		for(int i = 0; i < 6; i++)
+      		planeLayout.setSize(900,400);
+      		planeLayout.setResizable(true);	
+
+		for(int i = 0; i < 6; i++){
         		for(int j = 0; j < 12; j++)
      		 {
-       			 if(j < 4 && i != 3)
-			{
-				seats[i][j] = new Seats(200.00, "A" + ij + "");//first class
-				seats[i][j].addActionListener(this);
+       			 if(j < 4 && i != 3){
+				seats[i][j] = new Seat(200.00, "A" + i+j + "");//first class
+				   seats[i][j].setActionCommand(Integer.toString(i+j));
+				   seats[i][j].addActionListener(new ButtonClickListener());
+				   seats[i][j].setText(seats[i][j].getSeatName());
+				//seats[i][j].addActionListener(this);
 				panel.add(seats[i][j]);
-			}	
-			if((j >= 4 && j < 8) && i != 3)
-			{
-				seats[i][j] = new Seats(150.00, "B" + ij + "");//business
-				seats[i][j].addActionListener(this);
+				
+				}	
+
+			if((j >= 4 && j < 8) && i != 3){
+				seats[i][j] = new Seat(150.00, "B" + i+j + "");//business
+				seats[i][j].setActionCommand(Integer.toString(i+j));
+				seats[i][j].addActionListener(new ButtonClickListener());
+				seats[i][j].setText(seats[i][j].getSeatName());
+				//seats[i][j].addActionListener(this);
 				panel.add(seats[i][j]);
-			}
-			if(j > 8 && i != 3)
-			{
-				seats[i][j] = new Seats(100.00, "C" + ij + "");//economy
-				seats[i][j].addActionListener(this);
+				}
+			if(j >= 8 && i != 3){
+				seats[i][j] = new Seat(100.00, "C" + i+j + "");//economy
+				//seats[i][j].addActionListener(this);
+				seats[i][j].setActionCommand(Integer.toString(i+j));
+				seats[i][j].addActionListener(new ButtonClickListener());
+				seats[i][j].setText(seats[i][j].getSeatName());
+
 				panel.add(seats[i][j]);
-			}
-			if(i == 3)
-			{
-				seats[i][j] = new Seats(0, "");//emulates the row between seats on a plane
+				}
+
+			if(i == 3){
+				seats[i][j] = new Seat(0, "");//emulates the row between seats on a plane
 				seats[i][j].setEnabled(false);
 				seats[i][j].setAvailable(false);
-			}
-		add(panel);
-      		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		}//haven't tested if the name and stuff shows properly, i tested using my TTT and it looked like the format i expected but modifying it in the same way, so the logic SHOULD be correct lol.CG
+				seats[i][j].setBorderPainted(false);
+				panel.add(seats[i][j]);
+				}
+				
+		}
+		planeLayout.add(panel);
+      	planeLayout.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		}
+
+	/* When a seat is clicked and selected.*/
+		
+	}
 	/* When a seat is clicked and selected.*/
 	private class ButtonClickListener implements ActionListener{
-	public void actionPerformed(ActionEvent e) {
-		String command = e.getActionCommand();
-	
+		public void actionPerformed(ActionEvent e) {
+			String command = e.getActionCommand();
+		}
+	}
 	/* Main method to execute calls*/
 	public static void main(String[] args){
+		EventSeatingMap map = new EventSeatingMap();
+		map.checkVip();
+		map.getEvent();
 		
 	}
 }
